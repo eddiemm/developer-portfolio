c9ed067da3f3b2e22d594011e06b580c4eca5ecf
Edgar Mancia <eddiemm@users.noreply.github.com>
2022-06-10 14:03:56 -0700

    portfolio design complete

--- js/main.js
+++ js/main.js
@@ -1,9 +1,61 @@
-const menu = document.getElementById('menu');
+(function initialize(){
+    window.onload = () => {
+        
+        const menu = document.getElementById('menu');
+        const heroSection = document.getElementById('home');
+        const overviewSection = document.getElementById('overview');
+        const progressBar = document.getElementById('progressBar');
+        const downBtn = document.getElementsByClassName('btn-arrow-down')[0];
 
-const openMenu = () => {
-    menu.classList.add('open');
-}
+        const openMenu = () => {
+            if(window.innerWidth < 1024){
+                menu.classList.add('open');
+            }
+        }
+        
+        const closeMenu = () => {
+            if(window.innerWidth < 1024){
+                menu.classList.remove('open');
+            }
+        }
+        
+        const scrollToOverview = () => {
+            overviewSection.scrollIntoView(true);
+        }
+        
+        const showProgressBar = (evt) => {
+            let newProgressBarWidth = 0;
+            if(window.scrollY >= overviewSection.offsetTop){
+                // show progress bar
+                progressBar.style.top = 0 + 'px';
+        
+                // convert scroll length to progress bar width using linear conversion: scroll range to progress bar width range
+                
+                // the maximum scroll height is the entire html element height minus hero and 100vh 
+                // since we cannot scroll to the absolute bottom of the document
+                let scrollYMax = document.documentElement.scrollHeight - heroSection.offsetHeight - document.documentElement.clientHeight;
+                
+                let currrentScrollY = window.scrollY - heroSection.offsetHeight;
+               
+                let progressBarMaxWidth = document.documentElement.scrollWidth;
+                
+                let currentProgressBarWidth = progressBar.getBoundingClientRect().width;
+                
+                
+                // The formula used: newValue = (((oldVal - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin
+                newProgressBarWidth = parseInt((currrentScrollY * progressBarMaxWidth) / scrollYMax); 
+        
+                progressBar.style.width = newProgressBarWidth + 'px';
+            } else {
+                // hide progress
+                progressBar.style.top = -8 + 'px';
+            }
+        }
 
-const closeMenu = () => {
-    menu.classList.remove('open');
-}
+        downBtn.addEventListener('click', scrollToOverview, false);
+        
+        // adjust progress bar on window scroll and on resize
+        window.addEventListener('scroll', showProgressBar);
+        window.addEventListener('resize', showProgressBar);        
+    }
+}());
